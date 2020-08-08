61143b9598f416059236d5de5e48c57df6bf40fb
W-B28 <wtb2804@gmail.com>
2020-08-08 13:43:54 -0400

    MVP complete

--- client/src/components/PlantList.js
+++ client/src/components/PlantList.js
@@ -1,12 +1,30 @@
 import React, { Component } from "react";
 import axios from "axios";
 
-export default class PlantList extends Component {
+class PlantList extends Component {
   // add state with a property called "plants" - initialize as an empty array
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       plants: [],
+    }
+  }
+
+  componentDidMount(){ 
+        axios
+      .get('http://localhost:3333/plants')
+      .then(res => this.setState({plants: res.data.plantsData}))
+
+      .catch( err => 
+          console.error("Whoops an error was thrown:", err))
+  }
+
+  
 
   // when the component mounts:
   //   - fetch data from the server endpoint - http://localhost:3333/plants
-  //   - set the returned plants array to this.state.plants
+  //   - set the returned plants array to this.state.plantsnpm start
 
   /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
   render() {
@@ -37,3 +55,5 @@ export default class PlantList extends Component {
     );
   }
 }
+
+export default PlantList
